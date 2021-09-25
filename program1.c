bbcf2b6927869661121519f513b88421745aa344
Balaji-Kartheek <77656115+Balaji-Kartheek@users.noreply.github.com>
2021-09-25 20:02:01 +0530

    Add files via upload

---
+++ program1.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+int main()
+
+{
+
+    int a;      //input the first integer
+    int b;      //input the second integer
+    int sum;
+    printf("My name is Balaji kartheek(200968080)\n");      //name
+    printf("Enter the First Integer:\n");
+    scanf("%d",&a);         //read the first integer
+    printf("Enter the second Integer:\n");
+    scanf("%d",&b);         //read the second integer
+
+    sum=a+b;            //do the sum of a and b
+
+    printf("sum is : %d",sum);          //print their sum
+
+    return 0;
+
+}
